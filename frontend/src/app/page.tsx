31ff07e3874f7ae5f6a70640776495d8966c90d2
usman1121 <usmanabdella1121@gmail.com>
2026-04-14 13:41:07 +0300

    Initial commit: Employee Management System

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className=" text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            Welcome to Employee Management System
+          </h1>
+        
+        </div>
+
+      </main>
+    </div>
+  );
+}
